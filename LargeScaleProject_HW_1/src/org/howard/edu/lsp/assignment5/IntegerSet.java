115e9bb6cb5f98cce837bae9690f5362b3ac7cfd
HamidKab <160794949+HamidKab@users.noreply.github.com>
2026-04-05 23:16:17 -0400

    Changes to javadocs for assignment 5

--- LargeScaleProject_HW_1/src/org/howard/edu/lsp/assignment5/IntegerSet.java
+++ LargeScaleProject_HW_1/src/org/howard/edu/lsp/assignment5/IntegerSet.java
@@ -5,13 +5,12 @@ import java.util.Collections;
 import java.util.LinkedHashSet;
 
 /**
- * The following code represents an implementation of an IntegerSet class that provides various set operations 
- * such as union, intersection, difference, and complement. 
- * The class uses an ArrayList to store the unique integers in the set and includes methods for adding, removing, and checking for the presence of integers.
- * It also includes methods to find the largest and smallest integers in the set, as well as a method to clear the set and check if it is empty. 
- * The toString method provides a sorted representation of the set for easy visualization.
- * @author Generated
- * @Reviewer Hamid Kabia
+ * @description 
+ * The code below defines the class IntegerSet the performs the following operations on two integer sets
+ * Union, Intersect, Diff, Complement.
+ * The set is created using Javas ArrayList Util and well as the linkedHashSet to deal with duplicates
+ * @author Hamid Kabia
+ * 
  */
 
 public class IntegerSet {
@@ -28,6 +27,12 @@ public class IntegerSet {
         return set.size();
     }
 
+    /**
+     * 
+     * @param IntegerSet
+     * @description Check whether two sets are equal I.E [1, 2, 3] == [3, 1, 2]
+     * @return Bool True or False
+     */
     public boolean equals(IntegerSet b) {
         return set.containsAll(b.set) && b.set.containsAll(set);
     }
@@ -60,8 +65,10 @@ public class IntegerSet {
         set.remove(Integer.valueOf(item));
     }
 
-    /*
-    * Returns the union of this set and another set.
+    /**
+    @param IntegerSet the other IntegerSet to be combined with this set
+    @return IntegerSet result
+    *@description Returns the union of this set and another set.
     *creates a temporary LinkedHashSet to store the unique elements from both sets
     * It then adds elements from the temporary set to the result IntegerSet before returning it.
     * this is for duplication handling    
@@ -75,7 +82,10 @@ public class IntegerSet {
         return result;
     }
 
-    /* Returns the intersection of this set and another set using addAll and retainAll methods to find common elements
+    /**
+    * @param IntegerSet the other IntegerSet to be intersected with this set
+    * @description the intersection of this set and another set using addAll and retainAll methods to find common elements
+    * @return result
     */
     public IntegerSet intersect(IntegerSet intSetb) {
         IntegerSet result = new IntegerSet();
@@ -83,8 +93,10 @@ public class IntegerSet {
         result.set.retainAll(intSetb.set);
         return result;
     }
-    /*
-    * Returns the difference of this set and another set using addAll and removeAll methods to find elements that are in this set but not in the other.
+    /**
+    * @param IntegerSet
+    * @description the difference of this set and another set using addAll and removeAll methods to find elements that are in this set but not in the other.
+    * @return result
     */
     public IntegerSet diff(IntegerSet intSetb) {
         IntegerSet result = new IntegerSet();
@@ -92,8 +104,10 @@ public class IntegerSet {
         result.set.removeAll(intSetb.set);
         return result;
     }
-    /*
-    * Returns the complement of this set with respect to another set using addAll and removeAll methods.
+    /**
+    * @param IntegerSet
+    * @description the complement of this set with respect to another set using addAll and removeAll methods.
+    * @return result
     */
     public IntegerSet complement(IntegerSet intSetb) {
         IntegerSet result = new IntegerSet();
